1206c478a27864de7da0b618d2961abf34a0ea01
Deepika Shukla <76220585+deepika-shukla@users.noreply.github.com>
2026-05-02 13:02:27 +0530

    Update: burst-balloons - submission-0

--- Data Structures & Algorithms/burst-balloons/submission-0.py
+++ Data Structures & Algorithms/burst-balloons/submission-0.py
@@ -1,29 +1,24 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        #first we include 1 at each side
-        nums = [1] + nums + [1]
-        #cache the result in dp
+        # in this we will use the trick of 
+        # seeing what if we pop this baloon last, than
+        # our promblem will be divided into subproblem its left
+        # and right
+
         dp = {}
+        nums = [1] + nums + [1]
 
-        #recursive fucntion to check for each element
-        def dfs(l,r):
-            #if out of bounce
+        def dfs(l, r):
             if l > r:
-                return 0 #number of coins
-            
-            if (l,r) in dp: 
-                return dp[(l,r)]
-            
-            #if does not exist
+                return 0 # nothing left
+            if (l, r) in dp:
+                return dp[(l, r)]
             dp[(l,r)] = 0
-
-            #check for each element if it is popped at last
             for i in range(l, r+1):
                 coins = nums[l-1] * nums[i] * nums[r+1]
-                #now check for its left and right subarray
-                coins += dfs(l, i-1) + dfs(i+1,r)
-                #update the dp
+                coins += dfs(l, i-1) + dfs(i+1, r)
                 dp[(l,r)] = max(dp[(l,r)], coins)
-            return dp[(l,r)]
-        
-        return dfs(1, len(nums)-2)
+            return dp[(l, r)]
+            
+        return dfs(1, len(nums) - 2)
+    
