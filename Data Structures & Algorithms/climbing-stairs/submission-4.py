8086e16b8a354dfbfe7351a75949dd0008c17245
Deepika Shukla <76220585+deepika-shukla@users.noreply.github.com>
2026-05-23 11:22:43 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+
+        for i in range(1, n):
+            temp = two
+            two = one+two
+            one = temp
+        return two
