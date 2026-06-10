68e9d9ba52a85f13f13e31c2b7630b1f3d2021c5
Deepika Shukla <76220585+deepika-shukla@users.noreply.github.com>
2026-06-10 13:29:20 +0530

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+class PrefixTree:
+
+    def __init__(self):
+        self.node = TreeNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.node
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TreeNode()
+            cur = cur.children[c]
+        cur.end = True
+            
+
+
+    def search(self, word: str) -> bool:
+        cur = self.node
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.end
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.node
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+        
+        
